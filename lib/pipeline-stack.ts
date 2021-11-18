1cc41acb0dfbf23c0390896eedaae360b4ead03b
Freddie <pwed@users.noreply.github.com>
2021-11-18 23:21:51 +1100

    t

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -11,7 +11,7 @@ export class BlogPipelineStack extends Stack {
         input: pipelines.CodePipelineSource.connection('pwed/Blog', 'master', {connectionArn: 'arn:aws:codestar-connections:us-east-1:967803995830:connection/762f8358-181b-4602-8ec0-92982a01386f'}),
         commands: [
             "apt-get update && apt-get install -y neofetch",
-            "neofetch",
+            "neofetch", 
             "npm ci",
             "npm run build",
             "npx cdk synth"
