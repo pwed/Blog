18dd1dd1c1f1645a21556b4ae8164a723b43466b
Freddie <pwed@users.noreply.github.com>
2021-11-18 23:47:44 +1100

    deploy env

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,12 +1,13 @@
 import { Stage, StageProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { BlogStack } from './blog-stack';
-
+import { BlogStack } from "./blog-stack";
 
 export class BlogPipelineStage extends Stage {
-    constructor(scope: Construct, id: string, props?: StageProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StageProps) {
+    super(scope, id, props);
 
-        new BlogStack(this, 'BlogStack')
-    }
+    new BlogStack(this, "BlogStack", {
+      env: { account: "967803995830", region: "us-east-1" },
+    });
+  }
 }
